3a23703091cb8ef61b6fd1f359a1a984b0ed64eb
KafuuChino <2084049306@qq.com>
2022-04-04 21:30:45 +0800

    feat(menu): 自动选中

--- src/pages/Main/components/Menu.vue
+++ src/pages/Main/components/Menu.vue
@@ -46,8 +46,8 @@ import { IMenuClick } from '@/types/pages/Main'
 
 const store = useStore()
 
-const openKeys = ref([]) // 展开的菜单
-const selectedKeys = ref([]) // 当前选中的菜单项
+const openKeys = ref<string[]>([]) // 展开的菜单
+const selectedKeys = ref<string[]>([]) // 当前选中的菜单项
 const collapsed = ref(false) // 是否收起
 const menu_list = ref<IMenu[]>([]) // 菜单列表
 
@@ -56,11 +56,34 @@ const menu_click = (data: IMenuClick) => {
   push_route(data.key)
 }
 
+// 聚焦
+const select_menu = () => {
+  const path = window.localStorage.getItem('path')
+  store.state.Menu.menu.forEach((ele: IMenu) => {
+    if (ele.path === path) {
+      // 选中的一级菜单
+      selectedKeys.value = [path]
+      openKeys.value = []
+      return
+    }
+    if (ele.children) {
+      ele.children.forEach((child) => {
+        if (child.path === path) {
+          // 子元素匹配
+          selectedKeys.value = [path]
+          openKeys.value = [ele.path]
+        }
+      })
+    }
+  })
+}
+
 // 监控菜单
 watch(
   () => store.state.Menu.menu,
   (val) => {
     menu_list.value = val
+    select_menu()
   },
   { immediate: true, deep: true }
 )
