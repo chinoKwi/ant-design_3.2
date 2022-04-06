233423b151ec26d6ea3cc9b0ab5061f448452173
Chino <2084049306@qq.com>
2022-04-06 10:25:42 +0800

    feat(404): 增加样式

--- src/pages/404/404.vue
+++ src/pages/404/404.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <h1>This is 404 Page</h1>
+    <center>
+      <h1>404 Not Found</h1>
+      <hr />
+      <h2 class="mt-10px">ant-design_vue 后台管理框架 by chino</h2>
+    </center>
   </div>
 </template>
+<style lang="less" scoped>
+h1 {
+  font-size: 30px;
+  font-weight: bold;
+}
+</style>
